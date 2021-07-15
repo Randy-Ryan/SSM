1f56c76785fd428db7475bb0ff4e2cb8849b4916
Randy-Ryan <44271616+Randy-Ryan@users.noreply.github.com>
2021-07-14 20:04:13 -0400

    Randy updates. Note - no env variables are being pushed and the server must be hosted locally as of right now.

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,15 @@
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
 const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS
 
 const express = require('express')
@@ -23,8 +18,10 @@ const fs = require('fs')
 const stripe = require('stripe')(stripeSecretKey)
 
 var firebase = require('firebase');
-var firebaseConfig = {
-    apiKey: "AIzaSyDBLsz1BVWiJTB5HofoERqvW03G0ihMY5s",
+const cors = require('cors')({ origin: true });
+
+var config = {
+    apiKey:  process.env.FIREBASE_API_KEY,
     authDomain: "skateshoemate-3a12f.firebaseapp.com",
     databaseURL: "https://skateshoemate-3a12f-default-rtdb.firebaseio.com",
     projectId: "skateshoemate-3a12f",
@@ -32,71 +29,86 @@ var firebaseConfig = {
     messagingSenderId: "62694744898",
     appId: "1:62694744898:web:3dd732327ebd241a5a5edb",
     measurementId: "G-3CYN3Z5E83"
-}
-firebase.initializeApp(firebaseConfig)
-var ref = firebase.app().database().ref();
+};
+firebase.initializeApp(config);
 
-//update this db every 30 seconds
-//this should be changed so that when the shoe is posted - it will update directly
-(function loop() {
-    setTimeout(function () {
-      // execute script
-      ref.once('value')
-.then(function (snap) {
-    var obj = snap.val()
-    fs.writeFile ("input.json", JSON.stringify(obj), function(err) {
-        if (err) throw err;
-        console.log('updated');
-        }
-    );
-})
-      loop()
-    }, 30000); //9000 = 9000ms = 9s
-  }())
+// Get a reference to the database service
+var database = firebase.database();
+
+
+var shoes = database.ref()
 
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('../public'))
 
-app.get('/store', function (req, res) {
-    ref.once('value')
-    .then(function (snap) {
+// const path = require('path')
+
+// exports.store = functions.https.onRequest((req, res) => {
+//     console.log("hey")
+//     res.set('Access-Control-Allow-Origin', '*');
+
+//     if (req.method === 'OPTIONS') {
+//         // Send response to OPTIONS requests
+//         res.set('Access-Control-Allow-Methods', 'GET');
+//         res.set('Access-Control-Allow-Headers', 'Content-Type');
+//         res.set('Access-Control-Max-Age', '3600');
+//         res.status(204).send('');
+//       } else {
+//         console.log("lol")
+
+//     }
+
+
+
+// });
+
+app.get('/messages', (req, res) => {
+ 
+        res.render('messages.ejs', {
+            // stripePublicKey: stripePublicKey,
+            // items: snapshot.val().shoes
+        })
+})
+
+
+
+app.get('/store', (req, res) => {
+    shoes.on('value', (snapshot) => {
+        const data = snapshot.val();
         res.render('store.ejs', {
             stripePublicKey: stripePublicKey,
-            items: snap.val().shoes
+            items: snapshot.val().shoes
         })
-    })
+    });
 })
 
 app.post('/purchase', function (req, res) {
 
-    fs.readFile('input.json', function(error, data) {
-        if (error){
+
+    shoes.on('value', (snapshot) => {
+        const itemsArray = snapshot.val().shoes
+
+        let total = 0
+
+        req.body.items.forEach(function (item) {
+            const itemJson = itemsArray[parseInt(item.id)];
+            total = total + itemJson.price
+        })
+
+
+        stripe.charges.create({
+            amount: total,
+            source: req.body.stripeTokenId,
+            currency: 'usd'
+        }).then(function () {
+            console.log("Charge successful")
+            res.json({ message: 'Successfully purchased items' })
+        }).catch(function (error) {
+            console.log('Charge Fail' + error)
             res.status(500).end()
-        }
-        else{
-            const itemsJson = JSON.parse(data)
-            const itemsArray = itemsJson.shoes
-
-            let total = 0
-
-            req.body.items.forEach(function(item) {
-                const itemJson = itemsArray[parseInt(item.id)];
-                total = total + itemJson.price
-            })
-            stripe.charges.create({
-                amount: total,
-                source: req.body.stripeTokenId,
-                currency: 'usd'
-            }).then(function () {
-                console.log("Charge successful")
-                res.json({ message: 'Successfully purchased items' })
-            }).catch(function (error) {
-                console.log('Charge Fail' + error)
-                res.status(500).end()
-            }) 
-        }
+        })
     })
 })
 
