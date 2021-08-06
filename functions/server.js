c93a46fe4b0069f73ff711f7da969ff1e7e73347
Randy-Ryan <44271616+Randy-Ryan@users.noreply.github.com>
2021-08-06 11:35:43 -0400

    r

--- functions/server.js
+++ functions/server.js
@@ -226,8 +226,8 @@ app.post('/purchase', function (req, res) {
     })
 })
 
-const PORT = 8080;
+const PORT = 3000;
 const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST)
+app.listen(PORT)
 console.log(`Running on http://${HOST}:${PORT}`);
